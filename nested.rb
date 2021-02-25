660ab893edbbe612273344d1812f731dd38fb5d9
Ryan Zemlin <rzemlin@gmail.com>
2021-02-25 15:47:05 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -65,7 +66,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,8 +81,12 @@ def adding_matz
         }
      }
 
-    
-end
+		    programmer_hash[:yukihiro_matsumoto] = {}
+		    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		    programmer_hash
+		 end
+		 
 
 def changing_alan
 	programmer_hash = 
@@ -102,6 +107,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -120,6 +127,7 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+ 		}
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
