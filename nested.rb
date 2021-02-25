7885ccbf1de0ad656dd4f2f89b29665f9b5e000a
Ryan Zemlin <rzemlin@gmail.com>
2021-02-25 15:10:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -66,6 +67,8 @@ def adding_matz
 
 	programmer_hash = 
  		{
+       programmer_hash
+       {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,8 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+ programmer_hash[:yukihiro_matsumoto] = {}
+ programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+ programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+ programmer_hash
 end
 
 def changing_alan
