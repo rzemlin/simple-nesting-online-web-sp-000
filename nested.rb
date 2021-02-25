9a5ff9f6655b8842e7eb53d79f8289253c38f146
Ryan Zemlin <rzemlin@gmail.com>
2021-02-25 15:32:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
  programmer_hash.push[:yukihiro_matsumoto] = {}
  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
  programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+       }
  programmer_hash
 end
 
